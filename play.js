e14c887872e5906b30e7e1dee290423b8342cdd8
Brandon Felty <brcfelty@gmail.com>
2022-04-13 15:57:14 -0600

    worked for about 16 mins to setup standard input

--- play.js
+++ play.js
@@ -2,3 +2,25 @@ const connect = require('./client')
 
 console.log('Connecting ...');
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on('data', handleUserInput);
+
+  stdin.resume();
+  return stdin;
+};
+
+
+setupInput();
